b7e90feecb65b39505493b0ac70ee03cab10e708
Udoka <udochukwukaonyela@gmail.com>
2022-07-20 09:49:13 +0100

    fixes

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,8 +12,8 @@ const Home: NextPage = () => {
   const [file, setFile] = useState<any>(null);
 
   return (
-    <div className="flex items-center justify-center w-full min-h-screen bg-gray-300">
-    <div className="flex items-center justify-center bg-white p-6 w-9/12">
+    <div className="flex items-center justify-center w-full min-h-screen bg-gray-300 overflow-scroll">
+      <div className="flex items-center justify-center bg-white p-6 w-9/12 overflow-scroll">
 
       {!file ? <input className="file:hidden text-transparent after:content-['Drag_files_here_to_upload'] flex items-center justify-around pb-16 pt-12 after:align-middle after:my-auto cursor-pointer bg-gray-500 after:text-center p-auto after:box-border after:font-bold after:text-xl after:text-white" type={"file"} accept='image/*' onChange={(e: any) => setFile(URL.createObjectURL(e?.target?.files[0]))} />
         :
